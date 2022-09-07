2301f2f7a98391ddcb8755c6e21d17720f3ffeec
Syntria <egegermen2000@gmail.com>
2022-09-07 16:44:56 +0300

    New homewrok added

---
+++ SQL/Homework 8/Question-3.sql
@@ -0,0 +1,42 @@
+UPDATE employee
+SET id = 51,
+	name = 'Ali',
+	birthday = '2000-01-17',
+	email = 'ali@gmail.com'
+WHERE id = 10;
+
+
+
+UPDATE employee
+SET id = 52,
+	name = 'Veli',
+	birthday = '2000-02-17',
+	email = 'veli@gmail.com'
+WHERE id = 11;
+
+
+
+UPDATE employee
+SET id = 53,
+	name = 'Mahmut',
+	birthday = '1990-02-17',
+	email = 'veli@gmail.com'
+WHERE name = 'Jordon';
+
+
+
+UPDATE employee
+SET id = 54,
+	name = 'Mike',
+	birthday = '1999-09-26',
+	email = 'mike@gmail.com'
+WHERE birthday = '1981-09-26';
+
+
+
+UPDATE employee
+SET id = 54,
+	name = 'Jack',
+	birthday = '1989-10-26',
+	email = 'jack@gmail.com'
+WHERE email = 'delse2@cafepress.com';
---
+++ SQL/Homework 8/Question-4.sql
@@ -0,0 +1,22 @@
+DELETE FROM employee
+WHERE id = 4;
+
+
+
+DELETE FROM employee
+WHERE name = 'Hannis';
+
+
+
+DELETE FROM employee
+WHERE email = 'lbarron7@spotify.com';
+
+
+
+DELETE FROM employee
+WHERE birthday = '1975-01-26';
+
+
+
+DELETE FROM employee
+WHERE id = 23;
